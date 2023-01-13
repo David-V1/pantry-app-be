839167751eaf1293ef278f9922cc53a47fba2418
David Villaron <david.villaron@outlook.com>
2023-01-13 14:51:07 -0600

    BE-Account setup - POST

---
+++ src/main/java/net/yorksolutions/pantryappbe/DTO/AccountDTO.java
@@ -0,0 +1,11 @@
+package net.yorksolutions.pantryappbe.DTO;
+
+import java.util.Optional;
+
+public class AccountDTO {
+
+    Optional<Long> id;
+    public String email;
+    public String password;
+    public String familyName;
+}
---
+++ src/main/java/net/yorksolutions/pantryappbe/services/AccountService.java
@@ -0,0 +1,36 @@
+package net.yorksolutions.pantryappbe.services;
+
+import net.yorksolutions.pantryappbe.DTO.AccountDTO;
+import net.yorksolutions.pantryappbe.models.Account;
+import net.yorksolutions.pantryappbe.repositories.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    //Crud
+    public void createAccount(AccountDTO account) throws Exception {
+        //Check existence
+        Optional<Account> accountOptional = accountRepository.findByEmail(account.email);
+        if (accountOptional.isPresent()) {
+            throw new Exception("Account already exists");
+        }
+
+        Account newAccount = new Account();
+        newAccount.setEmail(account.email);
+        newAccount.setPassword(account.password);
+        newAccount.setFamilyName(account.familyName);
+        accountRepository.save(newAccount);
+    }
+
+    //cRud
+    //crUd
+    //cruD
+}
