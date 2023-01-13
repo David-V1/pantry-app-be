4722b5424515ec4779a536794611c47733b49508
David Villaron <david.villaron@outlook.com>
2023-01-13 11:36:49 -0600

    intial setup

---
+++ src/main/java/net/yorksolutions/pantryappbe/PantryAppBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.pantryappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantryAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/pantryappbe/PantryAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.pantryappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
